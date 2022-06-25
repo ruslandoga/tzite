c14359c88aeed0be4ec037d84032daed0d557c3f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-25 09:14:32 +0300

    cont

--- lib/tzdata_sqlite.ex
+++ lib/tzdata_sqlite.ex
@@ -8,14 +8,19 @@ defmodule TzdataSqlite do
   def download_tzdata do
     {:ok, conn} = Mint.HTTP.connect(:https, "data.iana.org", 443)
     {:ok, conn, ref} = Mint.HTTP.request(conn, "GET", "/time-zones/tzdata-latest.tar.gz", [], nil)
-    state = download_tzdata_loop(conn, ref, %{})
+    File.rm("tzdata-latest.tar.gz")
+    File.rm("tzdata-latest.tar.gz.part")
+    {:ok, fd} = File.open("tzdata-latest.tar.gz.part", [:binary, :append])
+    state = download_tzdata_loop(conn, ref, %{fd: fd})
     {:ok, _conn} = Mint.HTTP.close(conn)
+    File.close(fd)
+    File.rename!("tzdata-latest.tar.gz.part", "tzdata-latest.tar.gz")
 
     case state do
-      %{status: 200, headers: headers, data: data} ->
+      %{status: 200, headers: headers} ->
         case :proplists.get_value("content-type", headers) do
           "application/x-gzip" ->
-            :zlib.gunzip(data)
+            :erlang.garbage_collect(self())
         end
     end
   end
@@ -43,8 +48,11 @@ defmodule TzdataSqlite do
   defp process_responses([{:headers, ref, headers} | rest], ref, state),
     do: process_responses(rest, ref, Map.put(state, :headers, headers))
 
-  defp process_responses([{:data, ref, data} | rest], ref, state),
-    do: process_responses(rest, ref, Map.update(state, :data, data, fn prev -> prev <> data end))
+  # TODO unzip stream
+  defp process_responses([{:data, ref, data} | rest], ref, state) do
+    :ok = IO.binwrite(state.fd, data)
+    process_responses(rest, ref, state)
+  end
 
   defp process_responses([{:done, ref}], ref, state), do: {:done, state}
   defp process_responses([], _ref, state), do: {:cont, state}
