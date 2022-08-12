1099a4b28d9654094071c5c942bc54c1757cbb92
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-08-12 10:48:56 +0300

    Update load.exs

--- dev/load.exs
+++ dev/load.exs
@@ -2,8 +2,8 @@ alias Exqlite.Sqlite3
 
 Mix.install([:tzdata, :exqlite])
 :timer.sleep(:timer.seconds(5))
-tzdata = :ets.tab2list(:tzdata_rel_2022a)
-{:ok, conn} = Sqlite3.open("tzdata_2022a.db")
+tzdata = :ets.tab2list(:tzdata_rel_2022b)
+{:ok, conn} = Sqlite3.open("tzdata_2022b.db")
 
 :ok =
   Sqlite3.execute(
